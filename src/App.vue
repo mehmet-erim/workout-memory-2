4dad57c61c0ac4a87699533bcb4dce1472fb4363
mehmet-erim <bilgi@mehmeterim.com>
2020-06-14 18:45:36 +0300

    style: make layout view better

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,16 @@
 <template>
   <div id="app">
-    <ion-app>
+    <ion-app class="main">
       <ion-vue-router />
     </ion-app>
   </div>
 </template>
+
+<style>
+.main {
+  max-width: 425px;
+  margin: 0 auto;
+}
+</style>
+
+<script></script>
