d14c5c9270bb55bcecebeff94e3f5f0908c8133b
mehmet-erim <bilgi@mehmeterim.com>
2020-06-14 05:49:48 +0300

    chore: change view names

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,45 @@
 import Vue from 'vue';
-import Home from '../views/Home.vue';
+import Movements from '../views/Movements.vue';
 import { IonicVueRouter } from '@ionic/vue';
+import Application from '../layouts/Application.vue';
 
 Vue.use(IonicVueRouter);
 
-export default new IonicVueRouter({
+const router = new IonicVueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes: [
+    { path: '', redirect: '/movements' },
     {
-      path: '/',
-      name: 'home',
-      component: Home,
+      path: '',
+      component: Application,
+      children: [
+        {
+          path: '/movements',
+          name: 'movements',
+          component: Movements,
+        },
+        {
+          path: '/workouts',
+          name: 'workouts',
+          component: () =>
+            import(/* webpackChunkName: "workouts" */ '../views/Workouts.vue'),
+        },
+      ],
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/login',
+      name: 'login',
       component: () =>
-        import(/* webpackChunkName: "about" */ '../views/About.vue'),
+        import(/* webpackChunkName: "login" */ '../views/Login.vue'),
     },
   ],
 });
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = true;
+  if (to.name !== 'login' && !isAuthenticated) next({ name: 'login' });
+  else next();
+});
+
+export default router;
---
+++ src/views/Movements.vue
@@ -0,0 +1,24 @@
+<template>
+  <ion-button>Open Modal</ion-button>
+</template>
+
+<script>
+// import About from './About.vue';
+
+// export default {
+//   name: 'Home',
+//   methods: {
+//     showModal: async function() {
+//       const modal = await this.$ionic.modalController.create({
+//         component: About,
+//         componentProps: {
+//           propsData: {
+//             userData: Math.round(Math.random() * 100),
+//           },
+//         },
+//       });
+//       await modal.present();
+//     },
+//   },
+// };
+</script>
