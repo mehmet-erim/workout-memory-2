ba50f688390c8b80f98c8b393922dcbbe064cacc
mehmet-erim <bilgi@mehmeterim.com>
2020-06-14 05:49:05 +0300

    feat: create Login.vue

---
+++ src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    <ion-button>Login with Facebook</ion-button>
+  </div>
+</template>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
