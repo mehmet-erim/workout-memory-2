424f5156485f5e90049d2451e0cdd3072b1d3020
mehmet-erim <bilgi@mehmeterim.com>
2020-06-16 00:43:59 +0300

    feat: make Movements screen stable

--- src/views/Movements.vue
+++ src/views/Movements.vue
@@ -1,18 +1,32 @@
 <template>
-  <ion-list>
-    <ion-item-sliding v-for="movement in movements" :key="movement.key">
-      <ion-item>
-        <ion-label>{{ movement.value }}</ion-label>
-      </ion-item>
-      <ion-item-options side="end">
-        <ion-item-option @click="edit(movement)">Edit</ion-item-option>
-        <ion-item-option color="danger" @click="remove(movement)"
-          >Remove</ion-item-option
-        >
-      </ion-item-options>
-    </ion-item-sliding>
-  </ion-list>
+  <div>
+    <ion-list>
+      <ion-item-sliding v-for="movement in movements" :key="movement.key">
+        <ion-item>
+          <ion-label>{{ movement.value }}</ion-label>
+        </ion-item>
+        <ion-item-options side="end">
+          <ion-item-option @click="edit(movement)">Edit</ion-item-option>
+          <ion-item-option color="danger" @click="remove(movement)"
+            >Remove</ion-item-option
+          >
+        </ion-item-options>
+      </ion-item-sliding>
+    </ion-list>
+
+    <ion-fab-button @click="openModal">
+      <ion-icon name="add"></ion-icon>
+    </ion-fab-button>
+  </div>
 </template>
+
+<style>
+ion-fab-button {
+  position: fixed;
+  bottom: 10px;
+  right: 10px;
+}
+</style>
 
 <script>
 import { mapGetters } from 'vuex';
@@ -28,7 +42,26 @@ export default {
       this.openModal(item);
     },
     remove(item) {
-      console.warn(item);
+      this.$ionic.alertController
+        .create({
+          cssClass: 'my-custom-class',
+          header: 'Are you sure?',
+          message: `<strong>${item.value}</strong> will be deleted!`,
+          buttons: [
+            {
+              text: 'Cancel',
+              role: 'cancel',
+              cssClass: 'secondary',
+            },
+            {
+              text: 'Yes',
+              handler: () => {
+                this.$dbService.removeMovement(item.key);
+              },
+            },
+          ],
+        })
+        .then((a) => a.present());
     },
     openModal(movement) {
       return this.$ionic.modalController
@@ -39,7 +72,7 @@ export default {
               movement,
             },
             propsData: {
-              title: movement ? `Edit - ${movement.value}` : 'New',
+              title: movement?.key ? `Edit - ${movement.value}` : 'New',
             },
           },
         })
